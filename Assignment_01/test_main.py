c130f7f2c123fe895791ef324e137d9933148760
Amanda-Larson <94248408+Amanda-Larson@users.noreply.github.com>
2022-04-24 12:40:01 -0700

    Add files via upload

---
+++ Assignment_01/test_main.py
@@ -0,0 +1,29 @@
+# header
+# title
+# date
+# name
+#
+
+
+from users import UserCollection
+import main
+from unittest import TestCase
+
+
+#main tests
+class TestInitUser(TestCase):
+
+    def test_init_user_collection(self):
+        #test that it was instatiated?
+        #expect an instatiated object which has a database/empty dictionary
+        expected = {}
+        # actual = main.init_user_collection()
+        # message = 'test'
+        self.assertEqual(expected, main.init_user_collection().database)
+        # self.assertIsInstance(expected, actual, message)
+
+class TestInitStatus(TestCase):
+
+    def test_init_status_collection(self):
+        expected = {}
+        self.assertEqual(expected, main.init_status_collection().database)
---
+++ Assignment_01/test_users.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+import users
+
+class TestUsersCollection(TestCase):
+
+
+
+    def test_Users(self):
+        pass
+
+    def test_adduser(self):
+        self.database = {}
